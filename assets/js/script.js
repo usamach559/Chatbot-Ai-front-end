791b7db80c8ecdc72e376cbaf26135bbc34ceb0f
chaudharytouseef <chtosifashraf@gmail.com>
2024-11-06 14:30:16 +0500

    new commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,9 +57,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-document
-.querySelector(".navbar-toggler")
-.addEventListener("click", function () {
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.classList.toggle("show");
+document.querySelector('.navbar-toggler').addEventListener('click', function () {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('show');
 });
