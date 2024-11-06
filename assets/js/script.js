a46066caaa8873cb6a40a43557f352f4e57d2eab
chaudharytouseef <chtosifashraf@gmail.com>
2024-11-06 15:55:35 +0500

    new commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,22 @@ document.querySelector('.navbar-toggler').addEventListener('click', function () 
     const sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('show');
 });
+
+
+// eye icon js 
+function togglePassword() {
+    const passwordField = document.getElementById('password');
+    const toggleIcon = document.querySelector('.toggle-password');
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        toggleIcon.classList.remove('bi-eye');
+        toggleIcon.classList.add('bi-eye-slash');
+    } else {
+        passwordField.type = 'password';
+        toggleIcon.classList.remove('bi-eye-slash');
+        toggleIcon.classList.add('bi-eye');
+    }
+}
+
+
+// end
